9c9ecac23ed72b770eb8e88c0ac599704d833559
Rama Ammoshah <ic21b026@technikum-wien.at>
2023-06-26 00:26:19 +0200

    my cmakefile

---
+++ test/main.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <csv.h>
+#define CATCH_CONFIG_MAIN
+
+#define MAX_ROWS 1000
+#define MAX_COLS 10
+#define MAX_FIELD_LENGTH 100
+
+int main(int argc, char **argv) {
+
+	readCdr();
+    
+    return 0;
+}
