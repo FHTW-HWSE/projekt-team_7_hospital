2175fe06448c6cc117c2b0bbb2916cf3955732a6
wolverine1vie <132943701+wolverine1vie@users.noreply.github.com>
2023-07-02 18:27:35 +0200

    Update main.cpp
    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,25 @@
 #include <stdlib.h>
 #include "functions.cpp"
 
-struct patient {
-    int ID;
-    char name[20];
-    int seatplace;
-    bool priority;
-    bool ambulance;
-    struct patient* next;
-    struct patient* prev;
-};
-
-typedef struct patient patient_struct;
-
-void displayListIDAndName(patient_struct* headP);
-patient_struct* createPatient(int newPatientID, char* newPatientName, int seatplace, bool priority, bool ambulance, patient_struct* newPatientNext, patient_struct* newPatientPrev);
-void addPatientToList(patient_struct** headP, patient_struct* newPatient);
-char* trimWhiteSpace(char* str);
-bool isSeatFree(patient_struct* headP, int seatNumber);
-void FreeSeat(patient_struct* headP, int seatNumber);
-void PrintPatientsInfo(patient_struct* headP); // Funktion zum Drucken der Patienteninformationen hinzugefügt
+//struct patient {
+//    int ID;
+//    char name[20];
+//    int seatplace;
+//    bool priority;
+//    bool ambulance;
+//    struct patient* next;
+//    struct patient* prev;
+//};
+//
+//typedef struct patient patient_struct;
+//
+//void displayListIDAndName(patient_struct* headP);
+//patient_struct* createPatient(int newPatientID, char* newPatientName, int seatplace, bool priority, bool ambulance, patient_struct* newPatientNext, patient_struct* newPatientPrev);
+//void addPatientToList(patient_struct** headP, patient_struct* newPatient);
+//char* trimWhiteSpace(char* str);
+//bool isSeatFree(patient_struct* headP, int seatNumber);
+//void FreeSeat(patient_struct* headP, int seatNumber);
+//void PrintPatientsInfo(patient_struct* headP); // Funktion zum Drucken der Patienteninformationen hinzugefügt
 
 
 int main() {
@@ -88,4 +88,3 @@ int main() {
     fclose(fp);
     return 0;
 }
-
